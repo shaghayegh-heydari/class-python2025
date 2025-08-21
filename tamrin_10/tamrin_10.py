119f2462d17cca82553fd216329a555bab0f3239
shaghayeghheydari <shaghayeghheydari2021@gmail.com>
2025-08-21 10:59:02 -0700

    tamrin_10

---
+++ tamrin_10/tamrin_10.py
@@ -0,0 +1,6 @@
+noa=input("noa ozviat chist? (vip/no)")
+mablagh=int(input("mablagh chghadr ast?"))
+if noa=="vip" or mablagh > 500000 :
+    print(f"tahvil raygan .")
+else:
+    print("bayad bh peyc 50000 bedahad .")

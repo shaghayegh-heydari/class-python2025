e621bb8e100e6a0aaf0c89988076b8b55da1965b
shaghayeghheydari <shaghayeghheydari2021@gmail.com>
2025-08-28 01:04:18 -0700

    tamrin-1

---
+++ tamrin _1/tamrin_1.py
@@ -0,0 +1,7 @@
+weather=int(input("damay hava chand cheghadr ast ?"))
+if weather > 30 :
+   print(f"damay balay 30 hava garm ast")
+elif weather < 15 :
+   print(f"damay hava sard ast")
+else :
+   print(f"damay 15 ta 30 khoshayand")

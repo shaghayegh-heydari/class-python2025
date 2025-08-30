c4bff2afc1c2e705b3291e06bc0a45313c12daac
shaghayeghheydari <shaghayeghheydari2021@gmail.com>
2025-08-30 12:51:12 -0700

    tamrin dict_6

---
+++ tamrin_6/tamrin dict_6.py
@@ -0,0 +1,9 @@
+emergency_phone_numbers = {
+    "police": 110,
+    "ems": 115,
+    "fire": 125,
+    "vaja": 113
+}
+
+name = input("pleas enter your emergency(police/ems/fire/vaja)")
+print(emergency_phone_numbers[name])

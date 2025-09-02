62d03c6a303e53197946b9e795508588ca3ec7b4
shaghayeghheydari <shaghayeghheydari2021@gmail.com>
2025-09-02 02:23:50 -0700

    tamrin telegrlm_2

--- tamrin telegram/tamrin telegram_2.py
+++ tamrin telegram/tamrin telegram_2.py
@@ -1,1 +1,10 @@
 num = [1,2,3,4,5,6,7]
+evens = []
+odds = []
+for n in num :
+    if n % 2 == 0 :
+        evens.append( n )
+    else:
+        odds.append( n )
+print("evens:", evens)
+print("odds:" , odds)
--- tamrin telegram/tamrin telegrim_1.py
+++ tamrin telegram/tamrin telegrim_1.py
@@ -4,10 +4,4 @@ for s in my_words:
     if len (s) > 5 :
         num.append(s)
 
-print(num)      
-
-
-
-    
-    
-
+print(num)

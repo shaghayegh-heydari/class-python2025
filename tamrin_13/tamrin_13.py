89814f8bf31cde6e3bbfbc103bff78f177c58568
shaghayeghheydari <shaghayeghheydari2021@gmail.com>
2025-09-09 10:21:05 -0700

    tamrin_13.py

--- tamrin_13/tamrin_13.py
+++ tamrin_13/tamrin_13.py
@@ -3,9 +3,9 @@ bishtarin = [0]
 camtarin = [0]
 
 for dama in darage :
-    if dama > bishtarin :
+    if dama < bishtarin :
         bishtarin=dama
-    if dama < camtarin :
+    if dama > camtarin :
         camtarin=dama
 
 print(f"list dama {darage}")

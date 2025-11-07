0ca697510e9a15c7c948638566426803c3e9d2e8
shaghayeghheydari <shaghayeghheydari2021@gmail.com>
2025-11-07 05:43:02 -0800

    car dar class_3

--- car dar class/car dar class_3.py
+++ car dar class/car dar class_3.py
@@ -1,2 +1,13 @@
 class Food :
-    def __init__(self,)
+    def __init__(self, color , taste ,type) :
+        self.color = color
+        self.taste = taste
+        self.type = type
+
+    def info (self):
+        print (f"my food is {self.type}  it is {self.taste} and {self.color}")
+
+if __name__=="__main__" :
+    food1 = Food ("dark brown","sour","fesengan") 
+
+    food1.info()
